adec736eaba5df557cb048478a7ec7a004d884e7
AgilThoriq <agil4529@gmail.com>
2024-10-26 16:54:30 +0700

    pertemuan ke4

---
+++ database/seeders/BookshelfSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class BookshelfSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table("Bookshelves")->insert([
+            [
+                "code"=> "RAK01",
+                "name"=> "informatika",
+            ],[
+                "code"=> "RAK02",
+                "name"=> "Sipil",
+            ],[
+                "code"=> "RAK03",
+                "name"=> "Industri",
+            ]
+        ]);
+    }
+}
